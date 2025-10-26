3ebe32dfd8c30fdd11dbf0697ee9c3a8f1d3c77a
Kadi <KadyAlsaif@outlook.com>
2025-10-26 15:56:24 +0300

    add

--- test/extracted_code/row_30.js
+++ test/extracted_code/row_30.js
@@ -1,33 +1,3 @@
-html
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Details Form</title>
-</head>
-<body>
-    <form id="userForm">
-        Full Name: <input type="text" id="fullName" required><br><br>
-        Gender: 
-        <select id="gender" required>
-            <option value="">Select Gender</option>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-        </select><br><br>
-        Birth Date: <input type="date" id="birthDate" required><br><br>
-        Mobile Number: <input type="tel" id="mobileNumber" required><br><br>
-        Address: <input type="text" id="address" required><br><br>
-        Email: <input type="email" id="email" required><br><br>
-        Social Security Number: <input type="text" id="socialSecurityNumber" required><br><br>
-        <button type="submit">Submit</button>
-    </form>
-
-    <script src="validateForm.js"></script>
-</body>
-</html>
-script
 document.getElementById('userForm').addEventListener('submit', function(event) {
     let isValid = true;
     const fullName = document.getElementById('fullName').value;
