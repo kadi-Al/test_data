ffedd2bdbbaef4464cfcae2418244c946a60b41f
Kadi <KadyAlsaif@outlook.com>
2025-11-01 17:16:37 +0300

    add

--- test/extracted_code/row_2.java
+++ test/extracted_code/row_2.java
@@ -1,68 +1,32 @@
-xml
-<dependency>
-    <groupId>org.eclipse.jetty</groupId>
-    <artifactId>jetty-server</artifactId>
-    <version>9.4.43.v20210629</version>
-</dependency>
-<dependency>
-    <groupId>org.eclipse.jetty</groupId>
-    <artifactId>jetty-servlet</artifactId>
-    <version>9.4.43.v20210629</version>
-</dependency>
-import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.AbstractHandler;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public class PostRequestHandler extends AbstractHandler {
-    @Override
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        if (request.getMethod().equals("POST")) {
-            // Read the POST data
-            StringBuilder postData = new StringBuilder();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = request.getInputStream().read(buffer)) != -1) {
-                postData.append(new String(buffer, 0, bytesRead));
-            }
-
-            // Process the POST data (for demonstration, we'll just return it in the response)
-            response.setContentType("application/json");
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write("{\"message\": \"" + postData.toString() + "\"}");
-
-            // Mark this request as handled
-            baseRequest.setHandled(true);
-        } else {
-            // Handle other HTTP methods if needed
-            response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+public class ByteCopy {
+    public static void main(String[] args) {
+        byte[] source = new byte[500]; // Example source array with 500 bytes
+        for (int i = 0; i < 500; i++) {
+            source[i] = (byte) (i + 1); // Filling the source array with incremental values
+        }
+        
+        byte[] destination = new byte[256]; // Destination array of fixed size 256 bytes
+        copyBytes(destination, source, 256); // Copying up to 256 bytes from source to destination
+        
+        System.out.println("Copied " + destination.length + " bytes:");
+        for (int i = 0; i < destination.length; i++) {
+            System.out.print(destination[i] + " "); // Printing the copied bytes
+        }
+    }
+    
+    /**
+     * Copies a specified number of bytes from source array to destination array.
+     * @param dest The destination byte array where data will be copied.
+     * @param src The source byte array from which data will be copied.
+     * @param numBytesToCopy Number of bytes to copy from the source to the destination.
+     */
+    public static void copyBytes(byte[] dest, byte[] src, int numBytesToCopy) {
+        if (numBytesToCopy > src.length) {
+            numBytesToCopy = src.length; // If requested more bytes than available in source, adjust the number of bytes to copy
+        }
+        
+        for (int i = 0; i < numBytesToCopy; i++) {
+            dest[i] = src[i]; // Copying each byte from source to destination
         }
     }
 }
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
-public class PostRequestServer {
-    public static void main(String[] args) throws Exception {
-        // Create a Jetty server with a default handler (optional, for simplicity)
-        Server server = new Server(8080);
-
-        // Add a context that will handle POST requests using our custom handler
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-
-        // Add a servlet holder to the context, which maps to our handler
-        context.addServlet(new ServletHolder(new PostRequestHandler()), "/*");
-
-        // Start the Jetty server
-        server.start();
-        server.join();
-    }
-}
-bash
-curl -X POST -d '{"key": "value"}' http://localhost:8080/
