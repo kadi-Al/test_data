499fd36b64c007cd6be553d9332d007228cd704c
kadi-Al <KadyAlsaif@outlook.com>
2025-11-17 09:18:50 +0300

    remove

--- test/extracted_code/row_38.js
+++
@@ -1,9 +0,0 @@
-script
-function sanitizeString(inputStr) {
-    // Use a regular expression to match non-alphabetic characters
-    return inputStr.replace(/[^a-zA-Z]/g, '&');
-}
-
-// Example usage:
-let inputStr = "Hello* World123!";
-console.log(sanitizeString(inputStr)); // Output: "Hello& World&&&"
--- test/extracted_code/row_83.js
+++
@@ -1,8 +0,0 @@
-# Read input from the command line
-input_value = int(input())
-
-# Add 1000 to the input value
-result = input_value + 1000
-
-# Print the result
-print(result)
